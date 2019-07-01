a4d1d78954d4872f5d7a508b8f871ec3919d207e
18205919 <18205919@ucdconnect.ie>
2019-07-01 14:59:51 +0100

    views.py updated for weather

--- DublinBus/backend/views.py
+++ DublinBus/backend/views.py
@@ -8,13 +8,14 @@ from django.shortcuts import HttpResponse
 from rest_framework import viewsets
 from rest_framework import views
 from rest_framework.response import Response
+import pymysql
+from datetime import datetime, timedelta
+
 
 
 from .serializers import *
 from .models import *
 from .permissions import ApiPermissions
-
-
 
 class SearchByStop(views.APIView):
     """
@@ -30,13 +31,13 @@ class SearchByStop(views.APIView):
         stop_number = self.get_params("stopnumber")
         time = self.get_params("time")
         day = self.get_params("day")
-        # weather = self.get_weather(time, day) # Done by Niamh
+        weather = self.get_weather(time, day)
         # routes = self.get_routes(stop_number) # Done by Niamh
         # direction = self.get_direction(stop_number, routes) # Done by Niamh
         machine_learning_inputs = serialize_machine_learning_input(stop_number,
-                                                                    weather,
-                                                                    routes,
-                                                                    direction)
+                                                                   weather,
+                                                                   routes,
+                                                                   direction)
         # results = self.get_arrival_times(machine_learning_inputs)
         results_sorted = self.sort_results(results)
         # results_json = jsonify_results(results)
@@ -55,7 +56,27 @@ class SearchByStop(views.APIView):
         Input: time and date
         Output: weather conditions for prediction as json or dictionary
         """
-        weather = "Weather API results for time and date"
+        def hour_rounder(t):
+            """
+            Rounds to nearest hour by adding a timedelta hour if minute >= 30
+            Input: DateTime
+            Output: DateTime Rounded
+            """
+            return (t.replace(second=0, minute=0, hour=t.hour)
+                       +timedelta(hours=t.minute//30))
+
+        datetime = datetime.strptime(date+" "+time, '%d-%m-%Y %H:%M')
+        datetime=(hour_rounder(datetime))
+        date=datetime.strftime("%d-%m-%Y")
+        time=datetime.strftime("%H:%M")
+
+        sql = "SELECT * FROM website.forecast where date='s' and time='%s'"(%date, %time)
+        db = pymysql.connect(host="csi420-01-vm9.ucd.ie", port=3306 , user="niamh", passwd="comp47360jnnd", db="website")
+        cursor = db.cursor()
+        cursor.execute(sql)
+        weather = cursor.fetchall()
+        cursor.close()
+
         return weather
 
     def get_routes(self, stop_number):
@@ -85,7 +106,8 @@ class SearchByStop(views.APIView):
                                                         weather,
                                                         routes,
                                                         direction)
-        machine_learning_inputs = MachineLearningInputSerializer(machine_learning_inputs)
+        machine_learning_inputs = MachineLearningInputSerializer(
+            machine_learning_inputs)
         return machine_learning_inputs.data
 
     def get_arrival_times(self, machine_learning_inputs):
@@ -120,6 +142,7 @@ class SearchByRoute(SearchByStop):
     Search by route feature
     Inherits from search by stop
     """
+
     def get(self, request):
         return HttpResponse("<h1>SearchByRoute</h1>")
 
@@ -129,6 +152,7 @@ class SearchByDestination(SearchByStop):
     Search by destination feature
     Inherits from search by stop
     """
+
     def get(self, request):
         return HttpResponse("<h1>SearchByDestination</h1>")
 
@@ -143,7 +167,7 @@ class StopsView(viewsets.ModelViewSet):
     """
     Shows stops table
     """
-    # Define which serializer to use
+      # Define which serializer to use
     permission_classes = (ApiPermissions, )
     serializer_class = StopSerializer
     queryset = Stops.objects.all()
