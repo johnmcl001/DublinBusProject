19655068b6af46b400d464aa2a023e6e0edb6852
18205919 <18205919@ucdconnect.ie>
2019-07-16 14:09:17 +0100

    Routing done for one route

--- DublinBus/frontend/src/Components/Map.js
+++ DublinBus/frontend/src/Components/Map.js
@@ -1,11 +1,49 @@
-import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import { Map, GoogleApiWrapper, Marker, InfoWindow, Polyline } from "google-maps-react";
 import React, { Component } from "react";
 import "../Static/StyleSheet/Map.css";
-import * as stationData from "./stop_info.json";
+//import * as stationData from "./stop_info.json";
+import * as stationkeys from "./frontEndBusInfo.json";
+import * as markerslist from "./oneRoute.json";
 
 class MapContainer extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {},
+        markers: markerslist[0][0],
+
+      };
+      this.polyCoords= [];
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+       return nextState.markers != this.markers;
+        }
+
+    onMarkerClick = (props, marker, e) =>{
+        this.setState({
+          selectedPlace: props,
+          activeMarker: marker,
+          showingInfoWindow: true
+        });
+    }
+
+    onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+  addPolyline=(props)=>{
+    this.polyCoords.push(props);
+  }
+
+
   render() {
-    //{ console.log([stationData][0]) }
+
     return (
       <div id="map">
         <div className="d-none d-lg-block d-md-none">
@@ -15,16 +53,34 @@ class MapContainer extends Component {
           >
             <Map
               google={this.props.google}
-              zoom={8}
+              zoom={11}
               initialCenter={{ lat: 53.3501, lng: -6.2661 }}
             >
-            {Object.keys([stationData][0]).map((station) => (
-              //console.log(stationData[station].lat);
-              //console.log(stationData[station].long);
-              <Marker
-                key={stationData[station].stop}
-                position={{ lat: stationData[station].lat, lng: stationData[station].long}}/>
-              ))}
+            {this.state.markers.map((station) => {
+              //console.log(stationkeys[station]['name']);
+              this.addPolyline({ lat: stationkeys[station]['lat'], lng: stationkeys[station]['long']});
+
+              return <Marker
+                key={station}
+                name={stationkeys[station]['name']}
+                position={{ lat: stationkeys[station]['lat'], lng: stationkeys[station]['long']}}
+                onClick={this.onMarkerClick}
+                />
+              })}
+              <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}>
+          <div>
+            <p>{this.state.selectedPlace.name}</p>
+          </div>
+      </InfoWindow>
+
+      <Polyline
+          path={this.polyCoords}
+          strokeColor="#0000FF"
+          strokeOpacity={0.8}
+          strokeWeight={2} />
+
 
             </Map>
           </div>
