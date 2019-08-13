247ee70eda1362cd860b6b33000778354233f8a8
Deyan Chen <>
2019-08-13 16:47:28 +0100

    do css on result page search by destination

--- DublinBus/frontend/src/Components/Table.js
+++ DublinBus/frontend/src/Components/Table.js
@@ -19,57 +19,20 @@ const Table = ({data}) =>
 
             <div className="container border border-primary">
                 <div className="container ResultPageDestination  ">
-                    <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                        <li className="nav-item">
-                            <a
-                                className="nav-link active"
-                                id="pills-home-tab"
-                                data-toggle="pill"
-                                href="#pills-home"
-                                role="tab"
-                                aria-controls="pills-home"
-                                aria-selected="true"
-                            >
-                                {data[0].duration} Mins
-                            </a>
-                        </li>
-                        {/*
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                id="pills-profile-tab"
-                data-toggle="pill"
-                href="#pills-profile"
-                role="tab"
-                aria-controls="pills-profile"
-                aria-selected="false"
-              >
-                {data[1].duration} Mins
-              </a>
-          </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                id="pills-contact-tab"
-                data-toggle="pill"
-                href="#pills-contact"
-                role="tab"
-                aria-controls="pills-contact"
-                aria-selected="false"
-              >
-                {data[2].duration} Mins
-              </a>
-            </li>
-           */}
-                    </ul>
+
+                    <div>
+
+                        <div className="row DestinationLabels  ">
+
+                            <h5>Estimate Time:  {data[0].duration}  mins</h5>
+
+                        </div>
+                    </div>
+
+
 
                     <div className="tab-content" id="pills-tabContent">
-                        <div
-                            className="tab-pane fade show active"
-                            id="pills-home"
-                            role="tabpanel"
-                            aria-labelledby="pills-home-tab"
-                        >
+                        <div>
                             {data[0].directions.map((x, y) => (
                                 //    Loop throught
                                 <div key={y}>
@@ -106,90 +69,7 @@ const Table = ({data}) =>
                                 </p>
                             </div>
                         </div>
-                        {/*
-            <div
-              className="tab-pane fade"
-              id="pills-profile"
-              role="tabpanel"
-              aria-labelledby="pills-profile-tab"
-            >
-              {data[1].directions.map((x, y) => (
-                //    Loop throught
-                <div>
-                  <div className="row instruction  ">
-                    <p className="Icons_ResultPage ">
-                      {x.travel_mode == "WALKING" ? (
-                        <FaWalking className="Icon" />
-                      ) : (
-                        <FaBus className="Icon" />
-                      )}
-                    </p>
-                    <p
-                      className="instruction_text "
-                      key={y}
-                    >
-                      {x.instruction}
-                    </p>
-                    <p className="time_show " key={y}>
-                      {x.time} mins
-                    </p>
-                  </div>
-                                                                    <FaLevelDownAlt className="arrow_down_icon "/>
 
-                </div>
-              ))}
-                            <div className="row result_destination  " >
-                                <p className="Icons_destination ">
-                                    <FaMapMarkerAlt className="Icon"/>
-                                </p>
-
-                                <p className="destination_text ">
-                                    Your Destination
-                                </p>
-                            </div>
-            </div>
-            <div
-              className="tab-pane fade"
-              id="pills-contact"
-              role="tabpanel"
-              aria-labelledby="pills-contact-tab"
-            >
-              {data[2].directions.map((x, y) => (
-                //    Loop throught
-                <div>
-                  <div className="row instruction border border-secondary ">
-                    <p className="Icons_ResultPage border border-secondary">
-                      {x.travel_mode == "WALKING" ? (
-                        <FaWalking className="Icon" />
-                      ) : (
-                        <FaBus className="Icon" />
-                      )}
-                    </p>
-                    <p
-                      className="instruction_text border border-secondary"
-                      key={y}
-                    >
-                      {x.instruction}
-                    </p>
-                    <p className="time_show border border-secondary" key={y}>
-                      {x.time} mins
-                    </p>
-                  </div>
-                </div>
-              ))}
-                            <div className="row result_destination  " >
-                                <p className="Icons_destination ">
-                                    <FaMapMarkerAlt className="Icon"/>
-                                </p>
-
-                                <p className="destination_text ">
-                                    Your Destination
-                                </p>
-                            </div>
-                                                                                                <FaLevelDownAlt className="arrow_down_icon "/>
-
-            </div>
-                      */}
                     </div>
                 </div>
             </div>
